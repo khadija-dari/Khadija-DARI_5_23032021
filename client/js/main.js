befa716141a7e7ee6b853abb6bea5d4571daf2e5
khadija dari <khadija.dari95@gmail.com>
2021-08-19 13:38:21 +0200

    liste des articles

--- client/js/main.js
+++ client/js/main.js
@@ -2,4 +2,26 @@ const url = "http://localhost:3001/api/teddies";
 
 fetch(url).then((Response) =>
   Response.json().then((data) => {
-    console.log(data)}));
+    console.log(data)
+    for (let product of data) {
+      console.log(product);
+      let main_container = document.getElementById("main_container");
+
+      if (document.getElementById("main_container") != null) {
+        main_container.innerHTML += `
+        
+          <div class="col">
+            <div class="card">
+              <img src=${product.imageUrl} class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">${product.price}€</p>
+                <button type="button" class="btn btn-primary btn-sm">Description</button>
+                <button type="button" class="btn btn-secondary btn-sm">Ajouter au panier</button>
+              </div>
+            </div>
+          </div>`;
+      }
+    }
+
+  }));
