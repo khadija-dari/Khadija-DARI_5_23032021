79f825c954d75f8c5122dc0575b6121375e16db1
khadija dari <khadija.dari95@gmail.com>
2021-08-24 13:34:52 +0200

    Ajout fichier panier

--- client/js/main.js
+++ client/js/main.js
@@ -9,18 +9,18 @@ fetch(url).then((Response) =>
 
       if (document.getElementById("main_container") != null) {
         main_container.innerHTML += `
-
-          <div class="col">
-            <div class="card">
-              <img src=${product.imageUrl} class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.price / 100}€</p>
-                <button type="button" class="btn btn-primary btn-sm">Description</button>
-                <button type="button" class="btn btn-secondary btn-sm add-cart">Ajouter au panier</button>
+  
+            <div class="col">
+              <div class="card">
+                <img src=${product.imageUrl} class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${product.name}</h5>
+                  <p class="card-text">${product.price / 100}€</p>
+                  <button type="button" class="btn btn-primary btn-sm">Description</button>
+                  <button type="button" class="btn btn-secondary btn-sm add-cart">Ajouter au panier</button>
+                </div>
               </div>
-            </div>
-          </div>`;
+            </div>`;
       }
     }
     let product = data;
